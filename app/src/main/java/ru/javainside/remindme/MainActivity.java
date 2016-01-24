d12dd4b85413018b4a6ab4fdbcb362b2a5e4c2f6
JavaGrinko <JavaGrinko@gmail.com>
2016-01-24 19:40:20 +0300

    Меню в NavigationView

--- app/src/main/java/ru/javainside/remindme/MainActivity.java
+++ app/src/main/java/ru/javainside/remindme/MainActivity.java
@@ -1,12 +1,14 @@
 package ru.javainside.remindme;
 
 import android.os.Bundle;
+import android.support.design.widget.NavigationView;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 
 public class MainActivity extends AppCompatActivity {
 
     private Toolbar toolbar;
+    private NavigationView navigationView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -15,6 +17,12 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         initToolbar();
+        initNavigationView();
+    }
+
+    private void initNavigationView() {
+        navigationView = (NavigationView) findViewById(R.id.navigation);
+        navigationView.inflateMenu(R.menu.menu_navigation);
     }
 
     private void initToolbar() {
