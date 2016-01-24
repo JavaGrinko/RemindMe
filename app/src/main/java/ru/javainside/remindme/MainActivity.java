642615bd1f813a94c14927f1018bd9d8fe927246
JavaGrinko <JavaGrinko@gmail.com>
2016-01-24 18:49:43 +0300

    Сделал тулбар

--- app/src/main/java/ru/javainside/remindme/MainActivity.java
+++ app/src/main/java/ru/javainside/remindme/MainActivity.java
@@ -2,15 +2,23 @@ package ru.javainside.remindme;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
 
-/**
- * Created by User on 24.01.2016.
- */
 public class MainActivity extends Activity {
+
+    private Toolbar toolbar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        initToolbar();
+    }
+
+    private void initToolbar() {
+        toolbar = (Toolbar) findViewById(R.id.toolbar);
+        toolbar.setTitle(R.string.app_name);
+        toolbar.inflateMenu(R.menu.menu_main);
     }
 }
