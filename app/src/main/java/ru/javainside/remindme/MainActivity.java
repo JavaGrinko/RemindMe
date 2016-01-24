9b1c2ac4395caf4bc1c608f47b003ffd395292c5
JavaGrinko <JavaGrinko@gmail.com>
2016-01-24 21:33:53 +0300

    Работают кнопки в Navigation view

--- app/src/main/java/ru/javainside/remindme/MainActivity.java
+++ app/src/main/java/ru/javainside/remindme/MainActivity.java
@@ -4,8 +4,11 @@ import android.os.Bundle;
 import android.support.design.widget.NavigationView;
 import android.support.design.widget.TabLayout;
 import android.support.v4.view.ViewPager;
+import android.support.v4.widget.DrawerLayout;
+import android.support.v7.app.ActionBarDrawerToggle;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.view.MenuItem;
 
 import ru.javainside.remindme.adapter.TabLayoutFragmentPagerAdapter;
 
@@ -14,6 +17,7 @@ public class MainActivity extends AppCompatActivity {
     private Toolbar toolbar;
     private NavigationView navigationView;
     private ViewPager viewPager;
+    private DrawerLayout drawerLayout;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -21,9 +25,14 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        initDrawerLayout();
         initToolbar();
         initNavigationView();
         initViewPager();
+    }
+
+    private void initDrawerLayout() {
+        drawerLayout = (DrawerLayout) findViewById(R.id.drawer_layout);
     }
 
     private void initViewPager() {
@@ -39,11 +48,39 @@ public class MainActivity extends AppCompatActivity {
     private void initNavigationView() {
         navigationView = (NavigationView) findViewById(R.id.navigation);
         navigationView.inflateMenu(R.menu.menu_navigation);
+
+        ActionBarDrawerToggle drawerToggle = new ActionBarDrawerToggle(this, drawerLayout, toolbar, R.string.open, R.string.close);
+        drawerLayout.setDrawerListener(drawerToggle);
+        drawerToggle.syncState();
+
+        navigationView.setNavigationItemSelectedListener(new NavigationView.OnNavigationItemSelectedListener() {
+            @Override
+            public boolean onNavigationItemSelected(MenuItem item) {
+                drawerLayout.closeDrawers();
+
+                switch (item.getItemId()){
+                    case R.id.notifications:
+                        goToNotifications();
+                        break;
+                }
+                return false;
+            }
+        });
     }
 
     private void initToolbar() {
         toolbar = (Toolbar) findViewById(R.id.toolbar);
         toolbar.setTitle(R.string.app_name);
         toolbar.inflateMenu(R.menu.menu_main);
+        toolbar.setOnMenuItemClickListener(new Toolbar.OnMenuItemClickListener() {
+            @Override
+            public boolean onMenuItemClick(MenuItem item) {
+                return false;
+            }
+        });
+    }
+
+    private void goToNotifications() {
+        viewPager.setCurrentItem(0);
     }
 }
